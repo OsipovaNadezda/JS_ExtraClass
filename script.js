e784d33f355276b9948f3163230489b72be223ab
OsipovaNadezda <osipavanadezda@gmail.com>
2022-06-02 23:46:51 +0300

    correction of mistakes of lesson 02

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
- num = 266219;
-var arr = [],
+let num = 266219;
+let arr = [],
     numString = num.toString(),
     array=[];
 
 for (let i = 0, len = numString.length; i < len; i++) {
     arr.push(+numString.charAt(i));
 }
-var  multiplication = console.info(eval((arr).join('*')));
+let  multiplication = console.info(eval((arr).join('*')));
 let degree = multiplication**3;
 // var degree = Math.pow (multiplication, 3);
-
-var degreeString = degree.toString();
+let degreeString = degree.toString();
 
 for (let i = 0; i < 2; i++) {
     array.push(+numString.charAt(i));
