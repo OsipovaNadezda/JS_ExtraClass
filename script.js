490d4051e192239be0d8fd498d2b87038136de41
OsipovaNadezda <osipavanadezda@gmail.com>
2022-06-09 13:22:14 +0300

    lesson04:functions complicated task

--- script.js
+++ script.js
@@ -16,27 +16,45 @@
 // }
 // console.log(array);
 
-
 // let lang = ['ru', 'en'];
-let lang = prompt("Введите язык", "ru, en" );
+// let lang = prompt("Введите язык", "ru, en" );
 
-if (lang == "ru") {
-    console.log("Дни недели на русском языке");
-} else {
-    console.log("Дни недели на английском языке");
+// if (lang == "ru") {
+//     console.log("Дни недели на русском языке");
+// } else {
+//     console.log("Дни недели на английском языке");
+// }
+
+// switch (true){
+//     case lang == "ru":
+//         console.log("Дни недели на русском языке");
+//         break;
+//     case lang == "en":
+//         console.log("Дни недели на английском  языке");
+//         break;   
+// }
+
+// //task 2
+// let namePerson = prompt("Введите имя", "Артем, Александр или другое имя" );
+// let result = namePerson == "Артем" ? "Директор" : namePerson == "Александр" ? "преподаватель" : "студент";
+// console.log(result);
+let string = prompt("Введите что-нибудь");
+
+function stringValidation(str) {
+    if (typeof str === "string") {
+        str = str.trim();
+        let limit = 30;
+        let result;
+        if (str.length <= limit) {
+            result = str;
+            return result;
+        }else{
+            result = str + "...";
+            return result;
+        }
+    } else {
+        console.log("Введеное значение не является строкой");
+    }
 }
-
-switch (true){
-    case lang == "ru":
-        console.log("Дни недели на русском языке");
-        break;
-    case lang == "en":
-        console.log("Дни недели на английском  языке");
-        break;   
-}
-
-
-//task 2
-let namePerson = prompt("Введите имя", "Артем, Александр или другое имя" );
-let result = namePerson == "Артем" ? "Директор" : namePerson == "Александр" ? "преподаватель" : "студент";
-console.log(result);
+console.log(typeof str);
+console.log(stringValidation(string));
