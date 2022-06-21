b4b86041e8f1ae3031144dc8bf35620af35c1f94
OsipovaNadezda <osipavanadezda@gmail.com>
2022-06-21 20:39:41 +0300

    completed task №2 lesson07

--- script.js
+++ script.js
@@ -1,48 +1,21 @@
-// let num = 266219;
-// let arr = [],
-//     numString = num.toString(),
-//     array=[];
+const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+let day = new Date();
+let today = day.getDay() -1;
 
-// for (let i = 0, len = numString.length; i < len; i++) {
-//     arr.push(+numString.charAt(i));
-// }
-// let  multiplication = console.info(eval((arr).join('*')));
-// let degree = multiplication**3;
-// // var degree = Math.pow (multiplication, 3);
-// let degreeString = degree.toString();
-
-// for (let i = 0; i < 2; i++) {
-//     array.push(+numString.charAt(i));
-// }
-// console.log(array);
-
-// let full = getFullPrice(2, 3)
-
-lang = prompt("Введите язык", "ru, en" );
-
-if (lang == "ru") {
-    console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
-} else {
-    console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+function getWeekDay() {
+    let bodyEl = '';
+    week.forEach(function(item, index){
+        let el = item;
+        if (today === index){
+            el = `<b>${el}</b>`;
+        } 
+        if(index === 5 || index === 6) {
+            el = `<i>${el}</i>`;
+        } 
+        el = `${el}<br>`;
+        bodyEl += el;
+    })
+    return bodyEl;
 }
 
-switch (true){
-    case lang == "ru":
-        console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
-        break;
-    case lang == "en":
-        console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
-        break;   
-}
-
-let entryLang = prompt("Введите язык", "ru, en" );
-    lang = {
-    'ru' : ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],  
-    'en' : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-};
-console.log(lang[entryLang].join());
-
-//task 2
-let namePerson = prompt("Введите имя", "Артем, Александр или другое имя" );
-let result = namePerson == "Артем" ? "Директор" : namePerson == "Александр" ? "преподаватель" : "студент";
-console.log(result);
+document.write(getWeekDay());
