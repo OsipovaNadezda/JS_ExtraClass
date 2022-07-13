ace0cf8448da47b47c30cc2c9adea313fac35256
OsipovaNadezda <osipavanadezda@gmail.com>
2022-07-13 22:58:59 +0300

    Classes and extends

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-let day = new Date();
-let today = day.getDay() -1;
-
-function getWeekDay() {
-    let bodyEl = '';
-    week.forEach(function(item, index){
-        let el = item;
-        if (today === index){
-            el = `<b>${el}</b>`;
-        } 
-        if(index === 5 || index === 6) {
-            el = `<i>${el}</i>`;
-        } 
-        el = `${el}<br>`;
-        bodyEl += el;
-    })
-    return bodyEl;
+class First {
+    hello() {
+        console.log("Привет я метод родителя!");
+    }
 }
 
-document.write(getWeekDay());
+class Second extends First {
+    constructor() {
+        super();
+    }
+
+    hello() {
+        super.hello();
+        console.log("А я наследуемый метод!");
+    }
+}
+
+const second = new Second();
+second.hello();
